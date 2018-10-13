1c1423f166f32be0ea1b5a30f42fdb8b7bfc6526
ydebaz <33871532+ydebaz@users.noreply.github.com>
2018-10-13 12:18:48 +0300

    Create sleep_in.py

---
+++ warmup1/sleep_in.py
@@ -0,0 +1,9 @@
+def sleep_in(weekday, vacation):
+  if weekday==False and vacation==False:
+    return True
+  elif weekday==True and vacation==False:
+   return False
+  elif weekday==False and vacation==True:
+   return True
+  else :
+   return True
