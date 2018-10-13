08026be5ccec26c8d11314dc91fde31a308de761
ydebaz <33871532+ydebaz@users.noreply.github.com>
2018-10-13 12:49:04 +0300

    Create squirrel_play.py

---
+++ logic1/squirrel_play.py
@@ -0,0 +1,11 @@
+def squirrel_play(temp, is_summer):
+  if is_summer ==True:
+    if temp>59 and temp<101:
+      return True 
+    else :
+     return False 
+  else    :
+    if temp>59 and temp<91:
+      return True 
+    else :
+     return False 
