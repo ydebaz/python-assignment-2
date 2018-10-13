f3cb24d645aec2664898628928bf4272a9363ee1
ydebaz <33871532+ydebaz@users.noreply.github.com>
2018-10-13 12:48:22 +0300

    Create date_fashion.py

---
+++ logic1/date_fashion.py
@@ -0,0 +1,13 @@
+def date_fashion(you, date):
+  if you>7 or date>7:
+     
+     if you<3 or date<3:
+      return 0
+     else:
+      return 2
+    
+  else :
+    if you<3 or date<3:
+      return 0
+    else :
+     return 1
