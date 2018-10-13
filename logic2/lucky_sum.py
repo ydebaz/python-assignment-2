4e7da5371629f593bcce049838bc993f2b0589e0
ydebaz <33871532+ydebaz@users.noreply.github.com>
2018-10-13 12:54:00 +0300

    Create lucky_sum.py

---
+++ logic2/lucky_sum.py
@@ -0,0 +1,15 @@
+def lucky_sum(a, b, c):
+  y=0
+  if a!= 13:
+    y=y+a
+  else :
+    return y
+  if b!= 13:
+    y=y+b
+  else :
+    return y
+  if c!= 13:
+    y=y+c
+  else :
+    return y  
+  return y  
